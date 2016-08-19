f34d39781a9f09e90036e96e95aec79514b1d88a
Jake Rote <jrote1@me.com>
2016-08-19 14:25:08 +0100

    Working on intellisense

--- src/providers/pubspec_yaml_completion_item_provider.ts
+++ src/providers/pubspec_yaml_completion_item_provider.ts
@@ -5,29 +5,46 @@ import { Analyzer } from "../analysis/analyzer";
 import * as as from "../analysis/analysis_server_types";
 import { logError } from "../utils";
 
+var yaml = require("yaml-js");
+
 export class PubspecYamlCompletionItemProvider implements CompletionItemProvider {
 	provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Thenable<CompletionList> {
 		var rootCompletionItems = [
-			{name: "name", description: "Required for every package"},
-			{name: "version", description: "Required for packages that are hosted on pub.dartlang.org"},
-			{name: "description", description: "Required for packages that are hosted on pub.dartlang.org"},
-			{name: "author", description: "Optional"},
-			{name: "authors", description: "Optional"},
-			{name: "homepage", description: "Optional"},
-			{name: "documentation", description: "Optional. Can be used to automatically create documentation"},
-			{name: "dependencies", description: "Can be omitted if your package has no dependencies"},
-			{name: "dev_dependencies", description: "Can be omitted if your package has no dev dependencies"},
-			{name: "dependency_overrides", description: "Can be omitted if you do not need to override any dependencies"},
-			{name: "environment", description: "Optional. Can be used to require a specific version of the Dart SDK"},
-			{name: "executables", description: "Required for every package"},
-			{name: "publish_to", description: "Optional. Specify where to publish a package. The default is pub.dartlang.org. Specify none to prevent a package from being published"},
-			{name: "transformers", description: "Optional. Used to configure dart2js or other transformers"}
+			{ name: "name", description: "Required for every package" },
+			{ name: "version", description: "Required for packages that are hosted on pub.dartlang.org" },
+			{ name: "description", description: "Required for packages that are hosted on pub.dartlang.org" },
+			{ name: "author", description: "Optional" },
+			{ name: "authors", description: "Optional" },
+			{ name: "homepage", description: "Optional" },
+			{ name: "documentation", description: "Optional. Can be used to automatically create documentation" },
+			{ name: "dependencies", description: "Can be omitted if your package has no dependencies" },
+			{ name: "dev_dependencies", description: "Can be omitted if your package has no dev dependencies" },
+			{ name: "dependency_overrides", description: "Can be omitted if you do not need to override any dependencies" },
+			{ name: "environment", description: "Optional. Can be used to require a specific version of the Dart SDK" },
+			{ name: "executables", description: "Required for every package" },
+			{ name: "publish_to", description: "Optional. Specify where to publish a package. The default is pub.dartlang.org. Specify none to prevent a package from being published" },
+			{ name: "transformers", description: "Optional. Used to configure dart2js or other transformers" }
 		];
 
-		return Promise.resolve(new CompletionList(rootCompletionItems.map(function(item){
+		var isEndOfFile = document.positionAt(document.getText().length).isEqual(position);
+		var doc = yaml.compose(document.getText());
+		var rootKeys = doc == null ? [] : (<[]>doc.value).map(function(val){
+			var key = val[0];
+			return {
+				value: key.value,
+				line: key.start_mark.line,
+				column: key.start_mark.column,
+				endColumn: key.end_mark.column
+			};
+		});
+
+		if(!(isEndOfFile && position.character == 0) && !rootKeys.some(x=>x.line == position.line && x.column <= position.character && x.endColumn >= position.character))
+			return Promise.resolve(new CompletionList());
+
+		return Promise.resolve(new CompletionList(rootCompletionItems.filter(x=>!rootKeys.some(k=>k.value == x.name)).map(function (item) {
 			var result = new CompletionItem(item.name, CompletionItemKind.Keyword);
 			result.detail = item.description;
 			return result;
-		}),false));
+		}), false));
 	}
 }
--- test/providers/pubspec_yaml_completion_item_provider.test.ts
+++ test/providers/pubspec_yaml_completion_item_provider.test.ts
@@ -14,10 +14,10 @@ suite("Pubspec.yaml completetion item provider Tests", () => {
     // Defines a Mocha unit test
     test("When file is empty and cursor is at position 0, returns root items", () => {
         var provider = new completionItemProvider.PubspecYamlCompletionItemProvider();
-__filename
+
         let uri = vscode.Uri.file(path.join(path.dirname(fs.realpathSync(__filename)) + "/../../../test/providers/test_files", 'empty_pubspec.yaml'));
         return vscode.workspace.openTextDocument(uri).then((textDocument) => {
-            return provider.provideCompletionItems(textDocument, new vscode.Position(1, 0), null).then(function (result) {
+            return provider.provideCompletionItems(textDocument, new vscode.Position(0, 0), null).then(function (result) {
                 assert.equal(false, result.isIncomplete);
                 assert.equal("name", result.items[0].label)
             });
@@ -26,12 +26,48 @@ __filename
 
     test("When file has name and cursor is at line 2, returns root items without name", () => {
         var provider = new completionItemProvider.PubspecYamlCompletionItemProvider();
-__filename
+
         let uri = vscode.Uri.file(path.join(path.dirname(fs.realpathSync(__filename)) + "/../../../test/providers/test_files", 'with_name_pubspec.yaml'));
         return vscode.workspace.openTextDocument(uri).then((textDocument) => {
-            return provider.provideCompletionItems(textDocument, new vscode.Position(2, 0), null).then(function (result) {
+            return provider.provideCompletionItems(textDocument, new vscode.Position(1, 0), null).then(function (result) {
                 assert.equal(false, result.isIncomplete);
                 assert.notEqual("name", result.items[0].label)
+            });
+        });
+    });
+
+    test("When cursor is on value, returns no intellisense", () => {
+        var provider = new completionItemProvider.PubspecYamlCompletionItemProvider();
+
+        let uri = vscode.Uri.file(path.join(path.dirname(fs.realpathSync(__filename)) + "/../../../test/providers/test_files", 'with_name_pubspec.yaml'));
+        return vscode.workspace.openTextDocument(uri).then((textDocument) => {
+            return provider.provideCompletionItems(textDocument, new vscode.Position(0, 11), null).then(function (result) {
+                assert.equal(false, result.isIncomplete);
+                assert.equal(0, result.items.length)
+            });
+        });
+    });
+
+    test("When line starts with space, does not return intellisense", () => {
+        var provider = new completionItemProvider.PubspecYamlCompletionItemProvider();
+
+        let uri = vscode.Uri.file(path.join(path.dirname(fs.realpathSync(__filename)) + "/../../../test/providers/test_files", 'with_space.yaml'));
+        return vscode.workspace.openTextDocument(uri).then((textDocument) => {
+            return provider.provideCompletionItems(textDocument, new vscode.Position(1, 1), null).then(function (result) {
+                assert.equal(false, result.isIncomplete);
+                assert.equal(0, result.items.length)
+            });
+        });
+    });
+
+    test("When line starts with -, does not return intellisense", () => {
+        var provider = new completionItemProvider.PubspecYamlCompletionItemProvider();
+
+        let uri = vscode.Uri.file(path.join(path.dirname(fs.realpathSync(__filename)) + "/../../../test/providers/test_files", 'with_array.yaml'));
+        return vscode.workspace.openTextDocument(uri).then((textDocument) => {
+            return provider.provideCompletionItems(textDocument, new vscode.Position(2, 1), null).then(function (result) {
+                assert.equal(false, result.isIncomplete);
+                assert.equal(0, result.items.length)
             });
         });
     });
